de2ea66c7d5311e512fc0d35fb53524bfca85d73
NataliaCor <88517729+NataliaCor@users.noreply.github.com>
2023-05-07 22:35:53 -0500

    save folder

---
+++ db.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+    user: "desarrollo5",
+    password: "AVNS_islzzcql-bjBra_SKJl",
+    host: "db-mysql-hq5-do-user-12947639-0.b.db.ondigitalocean.com",
+    port: "25060",
+    database: "defaultdb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+function exportsDB(){
+    return con
+}
+module.exports={exportsDB}
