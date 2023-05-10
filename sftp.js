a1459d0eea3c56014c801f01f6fcfe004c957754
NataliaCor <88517729+NataliaCor@users.noreply.github.com>
2023-05-10 18:24:05 -0500

    try config all

--- sftp.js
+++ sftp.js
@@ -6,74 +6,7 @@ const { count } = require('console');
 const con = exportsDB();
 
 // ID_userS="3960020000112264857"
-//días que se van a dejar desde que se grabó la entrevista para cambiar de ubicación del archivo 
-diasAesperar="5";
-//Query par buscar registros con la ruta de digital, validando que la fecha sea anterior a hoy hace los "días a esperar"
-let sqlVideo = "SELECT `aplicar_convocatorias_id`,`preguntasRes` FROM defaultdb.entrevistas WHERE `ruta` LIKE '/mnt/entrevistavirtual/' AND  DATE(`fecha`) <= CURDATE()-"+diasAesperar+" ;"
-//ejecución de query
-con.query(sqlVideo, async function (err, result){
-  //si toma error imprimir en consola
-  if(err) console.log(err); 
-  //intentar proceso completo
-  try{
-  //para cada registro de entrevista que encontró identifique el id y la cantidad de preguntas almacenadas(preguntasRes)
-    for(index in result){
-      var listaVideos=[];
-      var id_=result[index]["aplicar_convocatorias_id"];
-      var numPreguntas=result[index]["preguntasRes"];
-      //para cada pregunta existente por entrevista crea ruta según parametrización (rutaDigitalocean/idRegistro_numeroPregunta.mp4)
-      for(var i= 1;i<numPreguntas+1;i++  ){
-        ruta="/mnt/entrevistavirtual/"+id_+"_"+i+".mp4";
-        listaVideos.push(ruta);
-        //Para cada ruta creada ejecutar clonacion de servidor digital Ocean a servidor físico HQ5
-        //routeHQ5L="./transfdhq5/"+id_+"_"+i+".mp4"
-        // try{
-        //   await client.uploadFile(ruta ,routeHQ5L);
-        // }
-        // catch(err){
-        //   console.log(err)
-        // }    
-      }
-      //actualizar ruta en SQL
-      // var sqlUpdate = "UPDATE `entrevistas` SET `ruta`= './transfdhq5/'  WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
-      // con.query(sqlUpdate, function (err, result) {
-      //   if (err) console.log(err); 
-      //   console.log("video guardado en servidor físico");
-      // })
-      // //test imprimir en consola id's encontrados y listas de rutas videos generadas
-      console.log(listaVideos)
-      console.log(result[index]["aplicar_convocatorias_id"])
-      //Eliminar alrchivos de servidor Digita Ocean
-      // for(_ruta in listaVideos){
-      //   fs.unlink(_ruta, (err) => {
-      //     if (err) {
-      //       console.error(err)
-      //       return
-      //     }
-      //     console.log("eliminado:" + _ruta)
-      //   })
-      // }
-    };
-  }
-  catch(err){
-    console.log(err);
-  }
-})
- 
 
-
-
-// var sql = "UPDATE `entrevistas` SET `ruta`= '" + id + "' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
-// try {
-//   con.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log("video guardado en db");
-//     resSQL = "succesfull query";
-//   }).then(sendZoho(req, respuestasConsol));
-// }
-// catch (error) {
-//   resSQL = error;
-// }
 
 
 // //-----------------------------------------
@@ -116,37 +49,68 @@ class SFTPClient {
     username: 'transfdhq5',
     password: 's*3/X26Qm'
   });
-  listTest=["/mnt/entrevistavirtual/3960020000012264857_4.mp4" ];
-  var countProcess=0;
-  idTest="3960020000012264857";
-  for (route_ in listTest){
-    countProcess++
-    try{
-      _ruta = listTest[route_];
-      archivoNombre="./transfdhq5/"+idTest+"_44.mp4";
 
-      try{
-        await client.uploadFile( _ruta,archivoNombre )
-        await fs.unlink(_ruta, (err) => {
-              if (err) {
-                console.error(err)
-                return
-              }
-             console.log("eliminado:" + _ruta)
-           })
-        await console.log("done")
+  //días que se van a dejar desde que se grabó la entrevista para cambiar de ubicación del archivo 
+diasAesperar="5";
+//Query par buscar registros con la ruta de digital, validando que la fecha sea anterior a hoy hace los "días a esperar"
+let sqlVideo = "SELECT `aplicar_convocatorias_id`,`preguntasRes` FROM defaultdb.entrevistas WHERE `ruta` LIKE '/mnt/entrevistavirtual/' AND  DATE(`fecha`) <= CURDATE()-"+diasAesperar+" ;"
+var sqlUpdate = "UPDATE `entrevistas` SET `ruta`= './transfdhq5/" + id + "' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
+//ejecución de query
+await con.query(sqlVideo, async function (err, result){
+  //si toma error imprimir en consola
+  if(err) console.log(err); 
+  //intentar proceso completo
+  try{
+  //para cada registro de entrevista que encontró identifique el id y la cantidad de preguntas almacenadas(preguntasRes)
+    for(index in result){
+      var listaVideos=[];
+      var id_=result[index]["aplicar_convocatorias_id"];
+      var numPreguntas=result[index]["preguntasRes"];
+      //para cada pregunta existente por entrevista crea ruta según parametrización (rutaDigitalocean/idRegistro_numeroPregunta.mp4)
+      for(var i= 1;i<numPreguntas+1;i++  ){
+        ruta="/mnt/entrevistavirtual/"+id_+"_"+i+".mp4";
+        listaVideos.push(ruta);
+        //Para cada ruta creada ejecutar clonacion de servidor digital Ocean a servidor físico HQ5
+        routeHQ5L="./transfdhq5/"+id_+"_"+i+".mp4"
+        try{
+          archivoNombre="./transfdhq5/"+idTest+"_44.mp4";  
+          try{
+            await client.uploadFile( ruta,routeHQ5L )
+           //Eliminar alrchivos de servidor Digita Ocean
+            await fs.unlink(ruta, (err) => {
+                  if (err) {
+                    console.error(err)
+                    return
+                  }
+                 console.log("eliminado:" + ruta)
+               })
+            await console.log("done")
+          }
+          catch(err){
+            console.log("no borrado ni almacenado: " +err)
+          }
+          
+        }
+        catch(err){
+          console.log(err)
+        }
       }
-      catch(err){
-        console.log("no borrado ni almacenado: " +err)
-      }
-      
-    
-      
-    }
-    catch(err){
-      console.log(err)
-    }
+      await con.query(sqlUpdate, function (err, result) {
+            if (err) throw err;
+            console.log("video guardado en db");
+            resSQL = "succesfull query";
+          }).then(sendZoho(req, respuestasConsol));
+
+      // //test imprimir en consola id's encontrados y listas de rutas videos generadas
+      console.log(listaVideos)
+      console.log(result[index]["aplicar_convocatorias_id"])
+    };
   }
+  catch(err){
+    console.log(err);
+  }
+})
+// 
   //-----
   // remoteFile="/www/entrevistaVirtHQ5";
   // localFile="/mnt/entrevistavirtual/";
