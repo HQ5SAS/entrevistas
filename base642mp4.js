2a06d01fe959e3e49d6673d6f406f0b38b8a3d06
Jazz <99265919+Desarrollo5HQ5@users.noreply.github.com>
2023-05-18 14:57:50 -0500

    print typeof

--- base642mp4.js
+++ base642mp4.js
@@ -17,7 +17,8 @@ con.query(sqlVideo, async function (err, result){
        try{
         for(var i= 1;i< 8;i++  ){
             
-            var base64Video_ = (result[index]["pregunta"+i.toString()]).toString;
+            var base64Video_ = result[index]["pregunta"+i.toString()];
+            console.log(typeof(base64Video_))
             console.log("pregunta"+i.toString())
             if(base64Video_ != "NULL"){
                 Video_toVolume = base64Video_.replace(/^data:(.*?);base64,/, ""); // 
