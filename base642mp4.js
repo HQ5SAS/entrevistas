0f9cb5396f51e525054a164a05d9bd85e7ea6908
Jazz <99265919+Desarrollo5HQ5@users.noreply.github.com>
2023-05-18 14:22:53 -0500

    aaaaaaaaaaa

--- base642mp4.js
+++ base642mp4.js
@@ -17,25 +17,25 @@ con.query(sqlVideo, async function (err, result){
        try{
         for(var i= 1;i< 8;i++  ){
             
-            var base64Video_ = result[index]["pregunta"+toString(i)];
+            var base64Video_ = result[index]["pregunta1"];
             console.log(base64Video_);
             console.log("iterance: "+i )
             console.log("pregunta"+i.toString())
-            if(base64Video_ != "NULL"){
-                Video_toVolume = base64Video_.replace(/^data:(.*?);base64,/, ""); // 
-                Video_toVolume = Video_toVolume.replace(/ /g, '+'); // 
+            // if(base64Video_ != "NULL"){
+            //     Video_toVolume = base64Video_.replace(/^data:(.*?);base64,/, ""); // 
+            //     Video_toVolume = Video_toVolume.replace(/ /g, '+'); // 
             
-                fs.writeFile(`/mnt/entrevistavirtual/`+id_+"_"+i+".mp4" , Video_toVolume, 'base64', function(err) {
-                    console.log(err);
-                });
-            }
+            //     fs.writeFile(`/mnt/entrevistavirtual/`+id_+"_"+i+".mp4" , Video_toVolume, 'base64', function(err) {
+            //         console.log(err);
+            //     });
+            // }
           
         }
-        var sqlUpdate = "UPDATE `entrevistas` SET `ruta`= '"+ ruta+ "' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
-        await con.query(sqlUpdate, function (err, result) {
-              if (err) throw err;
-              console.log("video guardado en db");
-            });
+        // var sqlUpdate = "UPDATE `entrevistas` SET `ruta`= '"+ ruta+ "' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
+        // await con.query(sqlUpdate, function (err, result) {
+        //       if (err) throw err;
+        //       console.log("video guardado en db");
+        //     });
        }
        catch(err){
         console.log(err);
