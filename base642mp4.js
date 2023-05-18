8a84f1271fcdc21336b2780a7ccda45531aaf207
Jazz <99265919+Desarrollo5HQ5@users.noreply.github.com>
2023-05-18 15:32:38 -0500

    aaaaaaaaaaaaa

--- base642mp4.js
+++ base642mp4.js
@@ -22,21 +22,21 @@ con.query(sqlVideo, async function (err, result){
            // base64Video_=JSON.stringify(base64Video_)
             //console.log(typeof(base64Video_))
             console.log("pregunta"+i)
-            // if(base64Video_ != "NULL"){
-            //     Video_toVolume = base64Video_.replace(/^data:(.*?);base64,/, ""); // 
-            //     Video_toVolume = Video_toVolume.replace(/ /g, '+'); // 
+            if(base64Video_ != "NULL"){
+                Video_toVolume = base64Video_.replace(/^data:(.*?);base64,/, ""); // 
+                Video_toVolume = Video_toVolume.replace(/ /g, '+'); // 
             
-            //     fs.writeFile(`/mnt/entrevistavirtual/`+id_+"_"+i+".mp4" , Video_toVolume, 'base64', function(err) {
-            //         console.log(err);
-            //     });
-            // }
+                fs.writeFile(`/mnt/entrevistavirtual/`+id_+"_"+i+".mp4" , Video_toVolume, 'base64', function(err) {
+                    console.log(err);
+                });
+            }
           
         }
-        // var sqlUpdate = "UPDATE `entrevistas` SET `ruta`= '"+ ruta+ "' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
-        // await con.query(sqlUpdate, function (err, result) {
-        //       if (err) throw err;
-        //       console.log("video guardado en db");
-        //     });
+        var sqlUpdate = "UPDATE `entrevistas` SET `ruta`= '"+ ruta+ "' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
+        await con.query(sqlUpdate, function (err, result) {
+              if (err) throw err;
+              console.log("video guardado en db");
+            });
        }
        catch(err){
         console.log(err);
