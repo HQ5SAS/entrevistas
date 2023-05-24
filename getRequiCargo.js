efa0d15fa43827f2e0c2fd165e2a655e40400362
NataliaCor <88517729+NataliaCor@users.noreply.github.com>
2023-05-24 16:32:27 -0500

    get old cargos and requis

---
+++ getRequiCargo.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const { exportsDB } = require("./db");
+const con = exportsDB();
+const { spawn, ChildProcess } = require("child_process");
+
+let sqlVideo = "SELECT `aplicar_convocatorias_id` FROM defaultdb.entrevistas where requisicion IS NULL OR requisicion = ''"
+
+
+async function python_getInfo(content, lista) {
+
+    //subproceso python fn
+    pythonProcess = spawn("python3", ["./zohoGetInf.py"]);
+    var python_response = "";
+
+    pythonProcess.stdout.on("data", function (data) {
+    python_response += data
+    });
+
+    pythonProcess.stderr.on('data', function (data) {
+    console.error(data.toString());
+    })
+
+    pythonProcess.stdout.on("end", function () {
+    listaP = allReplace(python_response)
+    lista(listaP)
+    });
+    pythonProcess.stdin.setEncoding = 'utf-8';
+    pythonProcess.stdin.write(JSON.stringify(content));
+    pythonProcess.stdin.end();
+
+}
+
+con.query(sqlVideo, async function (err, result){
+    //si toma error imprimir en consola
+    if(err) console.log(err); 
+    for(index in result){
+        var id_=result[index]["aplicar_convocatorias_id"];
+        console.log(id_)
+        //get id info
+        python_getInfo({ "key": "contenido", "id": id_ }, loadPage);
+        try{
+            requi = JSON.parse(list);
+             cargo = requi.pop();   
+             try{
+                var sqlUpdate = "UPDATE `entrevistas` SET `requisicion` = '"+requi+"', `cargo` = '"+cargo+"' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
+                await con.query(sqlUpdate, function (err, result) {
+                      if (err) throw err;
+                      console.log("guardado en db");
+                    });
+               }
+               catch(err){
+                console.log(err);
+               } 
+        }
+        catch(err){
+            console.log(err);
+        }
+        
+        //para cada pregunta existente por entrevista crea ruta según parametrización (rutaDigitalocean/idRegistro_numeroPregunta.mp4)
+       
+        
+  
+      };
+})
+
---
+++ zohoGetInf.py
@@ -0,0 +1,50 @@
+import sys
+import json
+import requests
+
+comando = sys.stdin.readline()
+json_input = json.loads(comando)
+id = json_input["id"]
+cargo=""
+lista=[]
+def zoho_api():
+    # Obtener acces token
+    tk_param = {
+        'refresh_token': '1000.6ae69ca138d2f6c5adba08e52b52b4f6.4d09d4c6009923c6d7d36e535f9f37b7',
+        'client_id': '1000.BXCXYLGQX0TPGT0B4KPR5NKV2RXK2U',
+        'client_secret': '10e319c31847a45291d7b79b5344ea3b8329738a17',
+        'grant_type': 'refresh_token'}
+    tk_js = requests.post('https://accounts.zoho.com/oauth/v2/token', params=tk_param)
+    tk_js = json.loads(tk_js.text)
+
+    acces_tk = tk_js["access_token"]
+    # print(acces_tk)
+    if (acces_tk):
+        # Realizar consulta de reportes
+        a_tk = {
+            'Authorization': 'Zoho-oauthtoken ' + acces_tk
+        }
+        # 'ID==3960020000012096751'
+        c_param = {
+            'criteria': 'ID=='+id+'"'
+        }
+        # ,params = param
+        c_js = requests.get('https://creator.zoho.com/api/v2/hq5colombia/hq5/report/Vista_General11', headers=a_tk,
+                            params=c_param)
+        c_js = json.loads(c_js.text)
+        # print(c_js)
+        c_code = c_js['code']
+        if (c_code == 3330):
+            print('err. ERROR:', c_js['message'])
+        elif (c_code == 3100):
+            print('err. Reporte no existente')
+        elif (c_code == 3000):
+            c_data = c_js['data']
+            cargo=c_data[0]['CARGO']['display_value']
+            requi= c_data[0]['REQUISICION_RELATED']['display_value']
+            lista.append(cargo)
+            lista.append(requi)
+            print(lista)
+        else:
+            print('err. ERROR:',c_js['message'])
+zoho_api()
