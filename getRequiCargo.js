bb2fd1a1dee533a12cedfea8f4444b241d63b8c3
NataliaCor <88517729+NataliaCor@users.noreply.github.com>
2023-05-25 01:38:56 -0500

    requi more time

--- getRequiCargo.js
+++ getRequiCargo.js
@@ -3,7 +3,7 @@ const { exportsDB } = require("./db");
 const con = exportsDB();
 const { spawn } = require("child_process");
 
-let sqlVideo = "SELECT `aplicar_convocatorias_id` FROM defaultdb.entrevistas where cargo IS NULL OR cargo = ''";
+let sqlVideo = "SELECT `aplicar_convocatorias_id` FROM defaultdb.entrevistas where requisicion IS NULL OR requisicion = ''";
 
 function python_getInfo(content) {
   return new Promise((resolve, reject) => {
@@ -20,7 +20,6 @@ function python_getInfo(content) {
     });
 
     pythonProcess.stdout.on("end", function () {
-      console.log(python_response);
       resolve(python_response);
     });
 
@@ -51,7 +50,7 @@ con.query(sqlVideo, async function (err, result) {
         const response = await python_getInfo({ "key": "contenido", "id": id_ });
         console.log(response);
 
-        var sqlUpdate = "UPDATE `entrevistas` SET `cargo` = '" + response + "' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
+        var sqlUpdate = "UPDATE `entrevistas` SET `requisicion` = '" + response + "' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
         con.query(sqlUpdate, function (err, result) {
           if (err) throw err;
           console.log("guardado en db");
@@ -64,6 +63,6 @@ con.query(sqlVideo, async function (err, result) {
     await proceso();
 
     // Esperar 3 segundos antes de continuar con el siguiente ID
-    await new Promise((resolve) => setTimeout(resolve, 6000));
+    await new Promise((resolve) => setTimeout(resolve, 10000));
   }
 });
