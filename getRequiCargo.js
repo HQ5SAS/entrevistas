4c7c3c010f9aa5cbef960ef5fda5717ebf8c24ed
NataliaCor <88517729+NataliaCor@users.noreply.github.com>
2023-05-25 00:23:18 -0500

    dsad

--- getRequiCargo.js
+++ getRequiCargo.js
@@ -43,9 +43,9 @@ con.query(sqlVideo, async function (err, result){
                     cargo = list_;
                     console.log(cargo)
                     // console.log("JSONParse: "+ requi)
-                    requi = cargo.pop();   
+                    //requi = cargo.pop();   
                     try{
-                        var sqlUpdate = "UPDATE `entrevistas` SET `requisicion` = '"+requi+"', `cargo` = '"+cargo+"' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
+                        var sqlUpdate = "UPDATE `entrevistas` SET `cargo` = '"+cargo+"' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
                         con.query(sqlUpdate, function (err, result) {
                             if (err) throw err;
                             console.log("guardado en db");
