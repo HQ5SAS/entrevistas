510b92f18127ab2ddf801df72ef473fe5ec74c6f
NataliaCor <88517729+NataliaCor@users.noreply.github.com>
2023-05-25 01:06:27 -0500

    fsdf

--- getRequiCargo.js
+++ getRequiCargo.js
@@ -1,66 +1,66 @@
 const fs = require('fs');
 const { exportsDB } = require("./db");
 const con = exportsDB();
-const { spawn, ChildProcess } = require("child_process");
+const { spawn } = require("child_process");
 
-let sqlVideo = "SELECT `aplicar_convocatorias_id` FROM defaultdb.entrevistas where cargo IS NULL OR requisicion = ''"
+let sqlVideo = "SELECT `aplicar_convocatorias_id` FROM defaultdb.entrevistas where cargo IS NULL OR requisicion = ''";
 
 function python_getInfo(content) {
-    return new Promise((resolve, reject) => {
-      const pythonProcess = spawn("python3", ["./zohoGetInf.py"]);
-      let python_response = "";
-  
-      pythonProcess.stdout.on("data", function (data) {
-        python_response += data;
-      });
-  
-      pythonProcess.stderr.on("data", function (data) {
-        console.error(data.toString());
-        reject(data.toString());
-      });
-  
-      pythonProcess.stdout.on("end", function () {
-        console.log(python_response);
-        resolve(python_response);
-      });
-  
-      pythonProcess.on("error", function (error) {
-        console.error(error);
-        reject(error);
-      });
-  
-      pythonProcess.stdin.write(JSON.stringify(content));
-      pythonProcess.stdin.end();
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn("python3", ["./zohoGetInf.py"]);
+    let python_response = "";
+
+    pythonProcess.stdout.on("data", function (data) {
+      python_response += data;
     });
+
+    pythonProcess.stderr.on("data", function (data) {
+      console.error(data.toString());
+      reject(data.toString());
+    });
+
+    pythonProcess.stdout.on("end", function () {
+      console.log(python_response);
+      resolve(python_response);
+    });
+
+    pythonProcess.on("error", function (error) {
+      console.error(error);
+      reject(error);
+    });
+
+    pythonProcess.stdin.write(JSON.stringify(content));
+    pythonProcess.stdin.end();
+  });
+}
+
+con.query(sqlVideo, async function (err, result) {
+  // Si ocurre un error, imprimir en consola
+  if (err) {
+    console.log(err);
+    return;
   }
-con.query(sqlVideo, async function (err, result){
-    //si toma error imprimir en consola
-    if(err) console.log(err); 
-    for(index in result){
-        //get id info    
-        function proceso(){
-            try{
-            var id_=result[index]["aplicar_convocatorias_id"];
-             console.log(id_)
-            python_getInfo({ "key": "contenido", "id": id_ }).then((response) => {
-                // Hacer algo con la respuesta de Python
-            console.log(response);  
-              
-            var sqlUpdate = "UPDATE `entrevistas` SET `cargo` = '"+response+"' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
-            con.query(sqlUpdate, function (err, result) {
-                if (err) throw err;
-                console.log("guardado en db");
-                });
 
-              })
-            }
-            catch(err){
-                console.log(err)
-            }
+  for (let index in result) {
+    // Obtener el id de la información
+    async function proceso() {
+      try {
+        var id_ = result[index]["aplicar_convocatorias_id"];
+        console.log(id_);
 
-        }
-        proceso();
+        const response = await python_getInfo({ "key": "contenido", "id": id_ });
+        console.log(response);
 
-      };
-})
+        var sqlUpdate = "UPDATE `entrevistas` SET `cargo` = '" + response + "' WHERE (`aplicar_convocatorias_id` = '" + id_ + "');";
+        con.query(sqlUpdate, function (err, result) {
+          if (err) throw err;
+          console.log("guardado en db");
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
 
+    await proceso();
+  }
+});
