5bf4f79047058f85d2daf37ac54de8f577376c11
NataliaCor <88517729+NataliaCor@users.noreply.github.com>
2023-05-24 23:54:43 -0500

    sad

--- getRequiCargo.js
+++ getRequiCargo.js
@@ -4,7 +4,7 @@ const con = exportsDB();
 const { spawn, ChildProcess } = require("child_process");
 
 // let sqlVideo = "SELECT `aplicar_convocatorias_id` FROM defaultdb.entrevistas where requisicion IS NULL OR requisicion = ''"
-let sqlVideo = "SELECT `aplicar_convocatorias_id` FROM defaultdb.entrevistas where aplicar_convocatorias_id = '396034840020119398'"
+let sqlVideo = "SELECT `aplicar_convocatorias_id` FROM defaultdb.entrevistas where aplicar_convocatorias_id = '3960020000075914707'"
 
 
  function python_getInfo(content) {
