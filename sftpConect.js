30e0da4c5215931a364df38b6b55938365d83ff0
Jazz <99265919+Desarrollo5HQ5@users.noreply.github.com>
2023-06-09 15:20:04 -0500

    4 ALL RECORDSS

--- sftpConect.js
+++ sftpConect.js
@@ -43,7 +43,7 @@ const client = new SFTPClient();
     username: 'transfdhq5',
     password: 's*3/X26Qm'
   });
-  const sqlIds = "SELECT `id` FROM defaultdb.antecedentes where id = 8198 ";
+  const sqlIds = "SELECT `id` FROM defaultdb.antecedentes where ruta IS NULL OR ruta = '' ";
 
   con.query(sqlIds, async function (err, result) {
     if (err) {
