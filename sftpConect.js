b75920bac2eda19685b5a61c1dd9b725113e9c5e
Jazz <99265919+Desarrollo5HQ5@users.noreply.github.com>
2023-06-09 14:59:24 -0500

    dghyd

--- sftpConect.js
+++ sftpConect.js
@@ -5,7 +5,6 @@ const { exportsDB } = require("./db");
 const { count } = require('console');
 const con = exportsDB();
 const path = require('path');
-
 // //-----------------------------------------
 
 class SFTPClient {
@@ -44,22 +43,74 @@ const client = new SFTPClient();
     username: 'transfdhq5',
     password: 's*3/X26Qm'
   });
-    // //--crear archivo en carpetalocal
-    // try{
-    //   archivoNombre="./transfdhq5/hunterx/"+id_+".mp4";  
-    //   try{
-    //     await client.uploadFile( archivoNombre, pdf )
+  const sqlIds = "SELECT `id` FROM defaultdb.antecedentes where id = 8198 ";
 
-    //     await console.log("done")
-    //   }
-    //   catch(err){
-    //     console.log("no borrado ni almacenado: " +err)
-    //   }
-      
-    // }
-    // catch(err){
-    //   console.log(err)
-    // }
+  con.query(sqlIds, async function (err, result) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  
+    for (const item of result) {
+      const id_ = item.id;
+      console.log(id_);
+  
+      try {
+        const sqlPdf = "SELECT `consolidado` FROM defaultdb.evidencia_antecedentes WHERE id = '"+id_+"'";
+        con.query(sqlPdf, [id_], function (error, res) {
+          if (error) {
+            console.log(error);
+            return;
+          }
+  
+          if (res.length > 0) {
+            const base64Pdf = res[0]['consolidado'].toString('utf8');
+  
+            if (typeof(base64Pdf) !== "object") {
+              const pdfData = base64Pdf.replace(/^data:(.*?);base64,/, "").replace(/ /g, '+');
+              const filePath = `./transfdhq5/hunterx/${id_}.pdf`;
+              const tempFile = `${id_}.pdf`;
+  
+              fs.writeFile(tempFile, pdfData, 'base64', async function(err) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  try {
+                    await client.uploadFile(tempFile, filePath);
+                    console.log("Archivo subido correctamente.");
+  
+                    if (fs.existsSync(tempFile)) {
+                      fs.unlink(tempFile, (error) => {
+                        if (error) {
+                          console.log(`Error al eliminar el archivo: ${error}`);
+                        } else {
+                          console.log('Archivo eliminado correctamente.');
+                        }
+                      });
+                    }
+                        var sqlUpdate = "UPDATE `antecedentes` SET `ruta`= '"+ filePath+ "' WHERE id = " + id_ + "";
+                      await con.query(sqlUpdate, function (err, result) {
+                          if (err) throw err;
+                      console.log("pdf guardado en db");
+                      });
+                      console.log("Archivo guardado exitosamente.");
+                  } catch (err) {
+                    console.log(err);
+                  }
+                }
+              });
+            } else {
+              console.log("No hay archivo disponible.");
+              console.log(base64Pdf);
+            }
+          } else {
+            console.log("No se encontraron resultados para la consulta.");
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
       
 })();
-module.exports=client;
